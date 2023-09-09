1d883838ba8a3ee8c33e78cca727ddd63c528cb3
Glitch (pipesmarks) <none>
2023-09-09 17:47:01 +0000

    Added support for nodeinfo

---
+++ src/routes/activitypub/nodeinfo.js
@@ -0,0 +1,53 @@
+import express from "express";
+import {instanceType, instanceVersion} from "../../util.js";
+
+export const router = express.Router();
+
+router.get("/", async function (req, res) {
+  let domain = req.app.get("domain");
+  
+  if (req.originalUrl == "/.well-known/nodeinfo") {
+    let thisNode = {
+      links: [
+        {
+          rel: "http://nodeinfo.diaspora.software/ns/schema/2.0",
+          href: `https://${domain}/nodeinfo/2.0`,
+        },
+      ],
+    };
+    res.json(thisNode);
+  }
+
+  if (req.originalUrl == "/nodeinfo/2.0") {
+    
+    const bookmarksDb = req.app.get("bookmarksDb");
+    let bookmarkcount = await bookmarksDb.getBookmarkCount();
+    
+    let nodeInfo = {
+      version: 2.0,
+      software: {
+        name: instanceType,
+        version: instanceVersion,
+      },
+      protocols: [
+        "activitypub"
+      ],
+      services: {
+        outbound: [],
+        inbound: []
+      },
+      usage: {
+        users: {
+          total: 1,
+          activeMonth: 1,
+          activeHalfyear: 1
+        },
+        localPosts: bookmarkcount,
+      },
+      openRegistrations: false,
+      metadata: {}
+    };
+    // name, version, and post stats should be dynamic
+    res.json(nodeInfo);
+  }
+});
